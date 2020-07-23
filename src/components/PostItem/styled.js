be9a617ea27e779617f0e41608482900ae50eb8e
marcos-12marcos@hotmail.com <marcos-12marcos@hotmail.com>
2020-07-23 10:34:35 -0300

    resize tagPost

--- src/components/PostItem/styled.js
+++ src/components/PostItem/styled.js
@@ -1,6 +1,6 @@
-import styled from "styled-components";
-import media from 'styled-media-query';
-import AniLink from 'gatsby-plugin-transition-link/AniLink';
+import styled from "styled-components"
+import media from "styled-media-query"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 export const PostItemLink = styled(AniLink)`
   color: var(--texts);
@@ -8,12 +8,12 @@ export const PostItemLink = styled(AniLink)`
   text-decoration: none;
   max-width: 950px;
   margin: 0 auto;
-  transition: all .2s ease-in-out;
+  transition: all 0.2s ease-in-out;
   &:hover {
     transform: translateY(-5px);
     color: var(--highlight);
   }
-`;
+`
 
 export const PostItemWrapper = styled.section`
   align-items: flex-start;
@@ -34,11 +34,12 @@ export const PostItemWrapper = styled.section`
   ${media.lessThan("small")`
     padding: 2rem 1rem;
   `}
-`;
+`
 
 export const PostItemTag = styled.div`
   align-items: center;
-  background-image: ${props => props.image ? `url(${props.image})` : `url('/assets/img/volcano.jpg')`};
+  background-image: ${props =>
+    props.image ? `url(${props.image})` : `url('/assets/img/volcano.jpg')`};
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
@@ -48,7 +49,7 @@ export const PostItemTag = styled.div`
   font-size: 1.3rem;
   font-weight: 700;
   justify-content: center;
-  min-height: 200px;
+  min-height: 170px;
   min-width: 30%;
   text-transform: uppercase;
 
@@ -62,30 +63,30 @@ export const PostItemTag = styled.div`
   ${media.lessThan("small")`
     min-height: 200px;
   `}
-`;
+`
 
 export const PostItemInfo = styled.div`
   display: flex;
   flex-direction: column;
   margin-left: 1.5rem;
-  ${media.lessThan("medium") `
+  ${media.lessThan("medium")`
     margin: 0;
   `}
-`;
+`
 
 export const PostItemDate = styled.time`
   font-size: 0.9rem;
   margin-top: 1rem;
-`;
+`
 
 export const PostItemTitle = styled.h1`
   font-size: 1.9rem;
   font-weight: 700;
   margin: 0.2rem 0 0.5rem;
-`;
+`
 
 export const PostItemDescription = styled.p`
   font-size: 1.2rem;
   font-weight: 300;
   line-height: 1.2;
-`;
+`
