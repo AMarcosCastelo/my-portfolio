e9de16e6a2a13627d7eca9856c9c2869c7f153eb
AMarcosCastelo <marcos-12marcos@hotmail.com>
2020-09-16 10:51:25 -0300

    update gatsby-remark-relative-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,8 +56,16 @@ module.exports = {
           {
             resolve: `gatsby-remark-relative-images`,
             options: {
-              name: `uploads`
-            }
+              // [Optional] The root of "media_folder" in your config.yml
+              // Defaults to "static"
+              staticFolderName: 'static',
+              // [Optional] Include the following fields, use dot notation for nested fields
+              // All fields are included by default
+              include: ['featured'],
+              // [Optional] Exclude the following fields, use dot notation for nested fields
+              // No fields are excluded by default
+              exclude: ['featured.skip'],
+            },
           },
           {
             resolve: `gatsby-remark-images`,
